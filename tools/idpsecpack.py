10a5f19b865dcb691109858d64499a2d6a4c0c7b
Rob Cameron <rwcameron@gmail.com>
2015-03-25 12:41:03 -0700

    updated idp tool to also install templates

--- tools/idpsecpack.py
+++ tools/idpsecpack.py
@@ -21,10 +21,43 @@ password = args.password
 junos_dev = Device(host=host, user=username, password=password)
 #now we can connect to the device
 junos_dev.open()
-#run the RPC to get the support information
+#run the RPC to download the security package
 download_result = junos_dev.rpc.request_idp_security_package_download()
 #output to a sting
 print ET.tostring(download_result,encoding="utf8", method="text")
+
+#REGEX to match the done condition
+matchDone = re.compile('Done;.*',re.MULTILINE)
+
+#loop through status until OK is seen
+while True:
+    download_status = junos_dev.rpc.request_idp_security_package_download(status=True)
+    print ET.tostring(download_status,encoding="utf8", method="text")
+    info = download_status.findtext('secpack-download-status-detail')
+    if matchDone.match(info):
+        print "Download completed"
+        break
+    time.sleep(2)
+
+#run the RPC to install the security package
+install_result = junos_dev.rpc.request_idp_security_package_install()
+#output to a sting
+print ET.tostring(install_result,encoding="utf8", method="text")
+
+#loop through status until OK is seen
+while True:
+    install_status = junos_dev.rpc.request_idp_security_package_install(status=True)
+    print ET.tostring(install_status,encoding="utf8", method="text")
+    info = install_status.findtext('secpack-status-detail')
+    if matchDone.match(info):
+        print "Installation completed"
+        break
+    time.sleep(2)
+
+#run the RPC to download the download policy templates
+download_tmpl_result = junos_dev.rpc.request_idp_security_package_download(policy_templates=True)
+#output to a sting
+print ET.tostring(download_tmpl_result,encoding="utf8", method="text")
 
 #loop through status until OK is seen
 matchDone = re.compile('Done;.*',re.MULTILINE)
@@ -38,7 +71,10 @@ while True:
         break
     time.sleep(2)
 
-install_result = junos_dev.rpc.request_idp_security_package_install()
+#run the RPC to install the download policy templates
+install_tmpl_result = junos_dev.rpc.request_idp_security_package_install(policy_templates=True)
+#output to a sting
+print ET.tostring(install_tmpl_result,encoding="utf8", method="text")
 
 #loop through status until OK is seen
 while True:
