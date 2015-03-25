f54786373a3e4dc4a992f768f24d807dea2fbb6c
Kurt Bales <kwbales@kwbales.net>
2015-03-24 18:11:06 -0700

    Added OSPF/Loopback config, move example python code

---
+++ examples/conf-ospf.py
@@ -0,0 +1,29 @@
+from jnpr.junos.utils.config import Config
+from jnpr.junos import Device
+
+
+dev = Device( user='netconf', host='172.16.0.1', password='test123' )
+dev.open()
+
+set_cfg = """
+set interfaces lo0.0 family inet address 10.255.255.1/32
+
+set security zones security-zone vpn host-inbound-traffic system-services all
+set security zones security-zone vpn host-inbound-traffic protocols ospf
+
+set protocols ospf area 0 interface st0.0
+set protocols ospf area 0 interface lo0.0 passive
+"""
+
+dev.bind(cu=Config)
+dev.cu
+dev.cu.lock()
+
+dev.cu.load(set_cfg, format='set')
+
+commit_diff = dev.cu.diff()
+print "Diff: %s" % (commit_diff)
+dev.cu.commit()
+dev.cu.unlock()
+
+dev.close()
